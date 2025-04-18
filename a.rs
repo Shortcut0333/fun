12186ef9d214d361dc27f8191a9280f4d48a7df2
Shortcut <wendy.r.harvey@outlook.com>
2025-04-18 13:36:03 +0100

    Add files via upload

---
+++ a.rs
@@ -0,0 +1,151 @@
+// A file showcasing basic "Hello World" examples in various programming languages
+
+// 1. **C**
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// 2. **C++**
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+// 3. **Java**
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// 4. **Python**
+print("Hello, World!")
+
+// 5. **JavaScript**
+console.log("Hello, World!");
+
+// 6. **Ruby**
+puts "Hello, World!"
+
+// 7. **Go**
+package main
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+
+// 8. **Rust**
+fn main() {
+    println!("Hello, World!");
+}
+
+// 9. **Swift**
+import Swift
+
+print("Hello, World!")
+
+// 10. **PHP**
+<?php
+echo "Hello, World!";
+?>
+
+// 11. **Kotlin**
+fun main() {
+    println("Hello, World!")
+}
+
+// 12. **Shell (Bash)**
+echo "Hello, World!"
+
+// 13. **Perl**
+print "Hello, World!\n";
+
+// 14. **Haskell**
+main = putStrLn "Hello, World!"
+
+// 15. **Lua**
+print("Hello, World!")
+
+// 16. **Objective-C**
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
+
+// 17. **TypeScript**
+console.log("Hello, World!");
+
+// 18. **R**
+print("Hello, World!\n")
+
+// 19. **Scala**
+object HelloWorld extends App {
+  println("Hello, World!")
+}
+
+// 20. **Visual Basic (VB.NET)**
+Module HelloWorld
+    Sub Main()
+        Console.WriteLine("Hello, World!")
+    End Sub
+End Module
+
+// 21. **F#**
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello, World!"
+    0 // Return an integer exit code
+
+// 22. **Delphi/Object Pascal**
+program HelloWorld;
+
+begin
+  Writeln('Hello, World!');
+end.
+
+// 23. **SQL**
+-- SQL does not have a traditional "Hello World" program.
+-- Instead, it would be a query:
+SELECT 'Hello, World!';
+
+
+// 24. **Prolog**
+:- initialization(main).
+main :- write('Hello, World!'), nl.
+
+// 25. **COBOL**
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HelloWorld.
+       PROCEDURE DIVISION.
+       DISPLAY "Hello, World!".
+       STOP RUN.
+
+// 26. **Racket (Scheme)**
+#lang racket
+(displayln "Hello, World!")
+
+// 27. **ActionScript**
+trace("Hello, World!");
+
+// 28. **Elixir**
+IO.puts("Hello, World!")
+
+// 29. **Hack**
+<?hh
+echo "Hello, World!";
+
+// 30. **Julia**
+println("Hello, World!")
